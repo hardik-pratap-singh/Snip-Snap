6b57ee62561599ca4cafab3e5da1f1dad682fab7
Hardik <hardik182002@gmail.com>
2023-04-09 16:11:10 +0530

    Improved UI of Add posts

--- client/src/components/AddPost/UploadWidget.jsx
+++ client/src/components/AddPost/UploadWidget.jsx
@@ -40,13 +40,82 @@
 
 // export default UploadWidget;
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import ReactPlayer from "react-player";
+
+// export default function App(props) {
+//   const [vid, setVid] = useState("");
+
+//   const {func} = props ; 
+
+//   const postDetails = (pic) => {
+//     if (pic === undefined) {
+//       alert("Not Found");
+
+//       return;
+//     }
+
+//     try {
+//       const data = new FormData();
+//       data.append("file", pic);
+//       data.append("upload_preset", "Short_video");
+//       data.append("cloud_name", "dcmqewoxn");
+
+//       fetch("https://api.cloudinary.com/v1_1/dcmqewoxn/video/upload", {
+//         method: "POST",
+//         body: data
+//       })
+//         .then((res) => res.json())
+//         .then((data) => {
+//           setVid(data.url);
+//           func(data.url);
+//           console.log(data);
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//         });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   return (
+//     // <div className="App">
+//       <div className="postformuploaddiv">
+//       {(!vid) ?
+//         <input
+//           type="file"
+//           accept="video/*"
+//           style={{ margin: "6px 0px" }}
+//           name="pic"
+//           onChange={(e) => {
+//             postDetails(e.target.files[0]);
+//           }}
+//         />
+//         :
+//           <ReactPlayer url={`${vid}`} playing={true} loop={true} />
+//         // </div>
+
+// }
+
+
+
+//     </div>
+//   );
+// }
+
+
+import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
+import { RingLoader } from "react-spinners";
 
 export default function App(props) {
   const [vid, setVid] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const {func} = props ; 
+
+  
 
   const postDetails = (pic) => {
     if (pic === undefined) {
@@ -55,7 +124,9 @@ export default function App(props) {
       return;
     }
 
+    setLoading(true);
     try {
+      
       const data = new FormData();
       data.append("file", pic);
       data.append("upload_preset", "Short_video");
@@ -63,43 +134,58 @@ export default function App(props) {
 
       fetch("https://api.cloudinary.com/v1_1/dcmqewoxn/video/upload", {
         method: "POST",
-        body: data
+        body: data,
+
       })
+
+
         .then((res) => res.json())
         .then((data) => {
           setVid(data.url);
           func(data.url);
           console.log(data);
+          setLoading(false)
         })
         .catch((err) => {
           console.log(err);
+          setLoading(false)
         });
     } catch (error) {
       console.log(error);
+      setLoading(false)
     }
+
   };
 
   return (
     // <div className="App">
-      <div className="postformuploaddiv">
-      {(!vid) ?
+      <div className="postformuploaddiv2">
+      {
+
+
+      //  {isLoading ? <div>Loading...</div> : null}
+        
+        (!vid) ?
+       
+        (loading)?<RingLoader color="#bc36d6" /> :  
         <input
-          type="file"
-          accept="video/*"
-          style={{ margin: "6px 0px" }}
-          name="pic"
-          onChange={(e) => {
-            postDetails(e.target.files[0]);
-          }}
-        />
-        :
-          <ReactPlayer url={`${vid}`} playing={true} loop={true} />
-        // </div>
-
-}
-
-
+        className="upload_widget_input_vid"
+        type="file"
+        accept="video/*"
+        style={{ margin: "0px 0px" }}
+        name="pic"
+        onChange={(e) => {
+          postDetails(e.target.files[0]);
+        }}
+      />
+        :<>
+        <ReactPlayer url={`${vid}`} playing={true} loop={true} className="upload_widget_react_player" playingwidth="418px" height="200px" controls={false} />
+        </>
+      }    
 
     </div>
+
+
+
   );
 }
